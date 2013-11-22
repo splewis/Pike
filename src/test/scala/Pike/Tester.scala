8c373bb556299a7515168ae799c09f30a9f05f1f
Sean Lewis <splewis@utexas.edu>
2013-11-21 22:54:16 -0600

    mov test code added

--- src/test/scala/Pike/Tester.scala
+++ src/test/scala/Pike/Tester.scala
@@ -1,24 +1,33 @@
 package Pike
 
-object Tester extends Pike {
-  def main(args: Array[String]): Unit = {
-    mov(15, "r1")
-    mov(23, "r2")
-    mov(1, "r3")
-    add("r1", "r2", "r3")
-    inc("r3")
+import scala.collection.mutable.ListBuffer
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
 
-    mov(1.5, "r4")
-    mov(1.25, "r5")
-    fadd("r4", "r5", "r6")
-    
-    
-    printRegisterInfo("r1")
-    printRegisterInfo("r2")
-    printRegisterInfo("r3")
-    printRegisterInfo("r4")
-    printRegisterInfo("r5")
-    printRegisterInfo("r6")
-    
+class TestSuite {
+
+  val epsilon: Double = 1e-8
+
+  @Test
+  def movTests() = {
+    movProgram.runner
   }
+  object movProgram extends Pike {
+    def runner() = {
+      mov(1, "r0")
+      mov(1.0, "r1")
+      mov(-1.00000006, "r2")
+      mov(10000, "r5")
+      mov(-12345, "r15")
+      run
+      assertEquals(1, getIntValue("r0"))
+      assertEquals(1.0, getDoubleValue("r1"), epsilon)
+      assertEquals(-1.00000006, getDoubleValue("r2"), epsilon)
+      assertEquals(10000, getIntValue("r5"))
+      assertEquals(-12345, getIntValue("r15"))
+    }
+  }
+
 }
+
