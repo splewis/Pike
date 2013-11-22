61abc3a138f9bbca2566db8c21f91b0fea0f5a0e
Sean Lewis <splewis@utexas.edu>
2013-11-21 21:47:21 -0600

    Reworked code structure from functions to case classes that are interpreted.

--- src/main/scala/Pike/Pike.scala
+++ src/main/scala/Pike/Pike.scala
@@ -1,19 +1,21 @@
 package Pike
 
 import java.util.IllegalFormatException
+import scala.collection.mutable.MutableList
 
 class Pike {
 
-  val NumRegisters: Int = 16
+  private val NumRegisters: Int = 16
+  private val registers = new Array[Register](NumRegisters)
 
-  type Subroutine = (() => Unit)
+  private var shouldKill = false
+  private var instructionNumber = 0
+  private var instructions = new MutableList[Instruction]()
 
   abstract sealed class Register
   case class IntRegister(value: Int) extends Register
   case class DoubleRegister(value: Double) extends Register
   case class StringRegister(value: String) extends Register
-
-  private val registers = new Array[Register](NumRegisters)
 
   private def getRegister(rName: String): Register = {
     return registers(getRegisterIndex(rName))
@@ -28,89 +30,137 @@ class Pike {
     return index
   }
 
-  def mov(value: Any, rName: String): Unit = {
-    var newReg: Register = null
-    value match {
-      case d: Int => newReg = new IntRegister(d)
-      case d: Float => newReg = new DoubleRegister(d)
-      case d: Double => newReg = new DoubleRegister(d)
-      case d: String => newReg = new StringRegister(d)
-      case _ => throw new RuntimeException("Illegal type put in " + rName);
+  private def nextInstruction(): Unit = {
+    goto(instructionNumber + 1)
+  }
+
+  abstract class Instruction {
+    instructions += this
+    def action(): Unit
+    def apply() = action()
+  }
+
+  case class mov(value: Any, rName: String) extends Instruction {
+    override def action() = {
+      var newReg: Register = null
+      value match {
+        case d: Int => newReg = new IntRegister(d)
+        case d: Float => newReg = new DoubleRegister(d)
+        case d: Double => newReg = new DoubleRegister(d)
+        case d: String => newReg = new StringRegister(d)
+        case _ => throw new RuntimeException("Illegal type put in " + rName);
+      }
+      registers(getRegisterIndex(rName)) = newReg
+      nextInstruction()
     }
-    registers(getRegisterIndex(rName)) = newReg
   }
 
-  private def binaryIntFunction(r1: String, r2: String, r3: String, f: (Int, Int) => Int): Unit = {
-    val reg1: Register = getRegister(r1)
-    val reg2: Register = getRegister(r2)
-    var err: Boolean = false
-
-    if (reg1.isInstanceOf[IntRegister]) {
-      if (reg2.isInstanceOf[IntRegister])
-        mov(f(reg1.asInstanceOf[IntRegister].value, reg2.asInstanceOf[IntRegister].value), r3)
-      else
-        err = true
-    } else {
-      err = true
-    }
-    if (err)
-      throw new RuntimeException("Type mismatch integer add" + r1 + " to " + r2)
+  case class jmp(n: Int) extends Instruction {
+    override def action() = goto(n)
   }
 
-  def add(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a + b)
-  }
-
-  def mod(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a % b)
-  }
-
-  def mul(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a * b)
-  }
-
-  def idiv(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a / b)
-  }
-
-  def binaryFloatFunction(r1: String, r2: String, r3: String, f: (Double, Double) => Double): Unit = {
-    val reg1: Register = getRegister(r1)
-    val reg2: Register = getRegister(r2)
-    var err: Boolean = false
-
-    if (reg1.isInstanceOf[DoubleRegister]) {
-      if (reg2.isInstanceOf[DoubleRegister])
-        mov(f(reg1.asInstanceOf[DoubleRegister].value, reg2.asInstanceOf[DoubleRegister].value), r3)
-      else
-        err = true
-
-    } else {
-      err = true
-    }
-
-    if (err)
-      throw new RuntimeException("Type mismatch floating add " + r1 + " to " + r2)
-  }
-
-  def fadd(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a + b)
-  }
-
-  def fmul(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a * b)
-  }
-
-  def fidiv(r1: String, r2: String, r3: String): Unit = {
-    binaryIntFunction(r1, r2, r3, (a, b) => a / b)
-  }
-
-  def inc(rName: String): Unit = {
-    val reg = getRegister(rName)
-    if (reg.isInstanceOf[IntRegister])
-      mov(reg.asInstanceOf[IntRegister].value + 1, rName)
+  private def goto(n: Int): Unit = {
+    instructionNumber = n
+    if (shouldKill || instructionNumber >= instructions.size || instructionNumber < 0)
+      println("Program terminated")
     else
-      throw new RuntimeException("Cannot increment non-Int register " + rName)
+      instructions(instructionNumber).action()
   }
+
+  def run(): Unit = instructions(0).action()
+
+  object kill extends Instruction {
+    override def action() = {} // i.e. do nothing
+  }
+
+  case class add(r1: String, r2: String, r3: String) extends Instruction {
+    override def action() = {
+      val reg1: Register = getRegister(r1)
+      val reg2: Register = getRegister(r2)
+      var err: Boolean = false
+
+      if (reg1.isInstanceOf[IntRegister] && reg2.isInstanceOf[IntRegister]) {
+        val sum: Int = reg1.asInstanceOf[IntRegister].value + reg2.asInstanceOf[IntRegister].value
+        val inst: Instruction = mov(sum, r3)
+        inst.action()
+      } else {
+        throw new RuntimeException("Type mismatch integer add" + r1 + " to " + r2)
+      }
+    }
+
+  }
+
+  //    private def binaryIntFunction(r1: String, r2: String, r3: String, f: (Int, Int) => Int): Unit = {
+  //      val reg1: Register = getRegister(r1)
+  //      val reg2: Register = getRegister(r2)
+  //      var err: Boolean = false
+  //  
+  //      if (reg1.isInstanceOf[IntRegister]) {
+  //        if (reg2.isInstanceOf[IntRegister])
+  //          _mov(f(reg1.asInstanceOf[IntRegister].value, reg2.asInstanceOf[IntRegister].value), r3)
+  //        else
+  //          err = true
+  //      } else {
+  //        err = true
+  //      }
+  //      if (err)
+  //        throw new RuntimeException("Type mismatch integer add" + r1 + " to " + r2)
+  //    }
+
+  //    private def _add(r1: String, r2: String, r3: String): Unit = {
+  //      binaryIntFunction(r1, r2, r3, (a, b) => a + b)
+  //    }
+  //
+  //  private def _mod(r1: String, r2: String, r3: String): Unit = {
+  //    binaryIntFunction(r1, r2, r3, (a, b) => a % b)
+  //  }
+  //
+  //  private def _mul(r1: String, r2: String, r3: String): Unit = {
+  //    binaryIntFunction(r1, r2, r3, (a, b) => a * b)
+  //  }
+  //
+  //  private def _idiv(r1: String, r2: String, r3: String): Unit = {
+  //    binaryIntFunction(r1, r2, r3, (a, b) => a / b)
+  //  }
+  //
+  //  private def binaryFloatFunction(r1: String, r2: String, r3: String, f: (Double, Double) => Double): Unit = {
+  //    val reg1: Register = getRegister(r1)
+  //    val reg2: Register = getRegister(r2)
+  //    var err: Boolean = false
+  //
+  //    if (reg1.isInstanceOf[DoubleRegister]) {
+  //      if (reg2.isInstanceOf[DoubleRegister])
+  //        _mov(f(reg1.asInstanceOf[DoubleRegister].value, reg2.asInstanceOf[DoubleRegister].value), r3)
+  //      else
+  //        err = true
+  //
+  //    } else {
+  //      err = true
+  //    }
+  //
+  //    if (err)
+  //      throw new RuntimeException("Type mismatch floating add " + r1 + " to " + r2)
+  //  }
+  //
+  //  private def _fadd(r1: String, r2: String, r3: String): Unit = {
+  //    binaryIntFunction(r1, r2, r3, (a, b) => a + b)
+  //  }
+  //
+  //  private def _fmul(r1: String, r2: String, r3: String): Unit = {
+  //    binaryIntFunction(r1, r2, r3, (a, b) => a * b)
+  //  }
+  //
+  //  private def _fidiv(r1: String, r2: String, r3: String): Unit = {
+  //    binaryIntFunction(r1, r2, r3, (a, b) => a / b)
+  //  }
+  //
+  //  private def _inc(rName: String): Unit = {
+  //    val reg = getRegister(rName)
+  //    if (reg.isInstanceOf[IntRegister])
+  //      _mov(reg.asInstanceOf[IntRegister].value + 1, rName)
+  //    else
+  //      throw new RuntimeException("Cannot increment non-Int register " + rName)
+  //  }
 
   def registerInfo(rName: String): String = {
     val reg: Register = getRegister(rName)
